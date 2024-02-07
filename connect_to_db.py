c1d6b3db7276dd7006a0c5cedf02ada0e22e91ed
horiaS <horia.scarlat02@gmail.com>
2024-02-07 02:43:51 +0200

    small fix

---
+++ connect_to_db.py
@@ -0,0 +1,11 @@
+# IMPORTS---------------------------------------------------------
+import mysql.connector
+# ----------------------------------------------------------------
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "password",
+        database = "database"
+    )
